d49af643d954158f12d1897f2e470a388d515ab2
A B <nothingwillgowrong@gmail.com>
2025-03-01 12:18:19 -0500

    Removed comments.

--- views/index.js
+++ views/index.js
@@ -4,15 +4,6 @@ filterByParty.addEventListener("input", filterResults);
 filterByProvince.addEventListener("input", filterResults);
 
 function filterResults(mp) {
-    // document.startViewTransition(() => {
-    //     for (const mp of document.querySelectorAll(".mp-card")) {
-    //         mp.hidden = 
-    //             filterByParty.value === "All" && filterByProvince.value === "All"
-    //                 ? false
-    //                 : (filterByProvince.value !== "All" && mp.dataset.province !== filterByProvince.value) ||
-    //                   (filterByParty.value !== "All" && mp.dataset.party !== filterByParty.value);
-    //     }
-    // });
     if (document.startViewTransition) {
         document.startViewTransition(() => {
             for (const mp of document.querySelectorAll(".mp-card")) {
